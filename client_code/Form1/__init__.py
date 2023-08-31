7b23b0a745ff940f4401051b4a4b4ab91c8fab6a
vaibhavisita@gmail.com <vaibhavisita@gmail.com>
2023-08-31 07:17:41 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,3 +7,10 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+  def getData(self, **event_args):
+    """This method is called when the button is clicked"""
+    output = anvil.server.call('getData', 
+                                self.question.text)
+    if getData:
+      self.outputData = output;
+    pass
