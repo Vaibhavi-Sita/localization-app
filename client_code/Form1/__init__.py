96f2ab6f9d76ac265a55e329d6df83a3aa1db3cc
vaibhavisita@gmail.com <vaibhavisita@gmail.com>
2023-09-06 19:15:49 +0000

    enable download feature

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.media
 import json
 
 class Form1(Form1Template):
@@ -9,21 +10,12 @@ class Form1(Form1Template):
 
     # Any code you write here will run before the form opens.
   def getData(self, **event_args):
-    """This method is called when the button is clicked"""
-    # output = anvil.server.call('getData', 
-    #                             self.question.text)
-    # if getData:
-    #   self.outputData = output;
-
-    self.outputData.text = self.update_json(self.oldData, self.newData)
-    # self.outputData.text = self.update(self.oldData, self.newData)
-    
+    """This method is called when the submit button is clicked"""
+    self.outputData.text = self.update_json(self.oldData, self.newData)    
     pass
 
   def update_json(self, old_data, new_data):
-    # with open(old_file_path, 'r') as old_file, open(new_file_path, 'r') as new_file:
-    #     old_data = json.load(old_file)
-    #     new_data = json.load(new_file)
+    
     old_data = json.loads(old_data.text)
     new_data = json.loads(new_data.text)
     updated_data = self.update_json_values(old_data, new_data)
@@ -52,23 +44,8 @@ class Form1(Form1Template):
       else:
           return new_data
 
-  def update(self, old_json, new_json):
-    updated_json = {}
-    old_json = json.loads(old_json.text)
-    new_json = json.loads(new_json.text)
-    # Iterate through keys in the new JSON
-    for key, new_value in new_json.items():
-        if key in old_json:
-            old_value = old_json[key]
-            if isinstance(new_value, dict) and isinstance(old_value, dict):
-                # Recursively update nested dictionaries
-                updated_json[key] = update_json(old_value, new_value)
-            else:
-                # Keep the old value for unchanged keys
-                updated_json[key] = old_value
-        else:
-            # Add new keys to the updated JSON
-            updated_json[key] = new_value
-    
-    return json.dumps(updated_json)
+  def download(self, **event_args):
+    """This method is called when the downlaod button is clicked"""
+    # BlobMedia("text/plain", self.outputData, [name=output])
+    anvil.media.download(self.outputData.text) 
   
