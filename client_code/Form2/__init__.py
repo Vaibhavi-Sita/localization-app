8ee5319fa2ff17e2b7018f472327b47989ca5729
vaibhavisita@gmail.com <vaibhavisita@gmail.com>
2023-09-12 18:53:55 +0000

    Updated Directory Mode

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -6,7 +6,7 @@ from anvil.tables import app_tables
 import anvil.server
 import anvil.media
 import json
-import shutil
+# import shutil
 
 class Form2(Form2Template):
   def __init__(self, **properties):
@@ -15,55 +15,41 @@ class Form2(Form2Template):
 
     # Any code you write here will run before the form opens.
    
-  def getFiles(self):
+  def getFiles(self, **event_args):
     # self.process_directory(self.oldFiles, self.newFiles, self.outputFiles)
-    self.process_directory(self.oldFilesUpload, self.newFilesUpload, self.output_dir)
+    self.process_directory(self.oldFilesUpload, self.newFilesUpload)
     pass
 
-  def process_directory(self, oldFilesUpload, newFilesUpload, output_dir):
-    os.makedirs(output_dir, exist_ok=True)
+  def process_directory(self, oldFilesUpload, newFilesUpload):
+    # os.makedirs(output_dir, exist_ok=True)
     
     # List all files in the old and new directory
 
-    for old_file, new_file in oldFilesUpload.files, newFileUpload.files:
+    # for old_file, new_file in oldFilesUpload.files, newFilesUpload.files:
+    for old_file in oldFilesUpload.files:
+      for new_file in newFilesUpload.files:
         # Check if the file exists in the new directory
         if old_file.name == new_file.name:
-          old_json = json.load(old_file)
-          new_json = json.load(new_file)
+          # old_json = json.load(old_file)
+          # new_json = json.load(new_file)
+          old_json = json.loads(old_file.get_bytes())
+          new_json = json.loads(new_file.get_bytes())
+
           
-          updated_json = update_json(old_json, new_json)
+          updated_json = self.update_json(old_json, new_json)
+          # self.outputArea.text += json.dumps(updated_json)
+          # self.outputArea.text = json.loads(updated_json)
+          
+          updatedJsonFile = anvil.BlobMedia(content_type="text/plain", content=json.loads(updated_json).encode(), name=new_file.name)
+          anvil.media.download(updatedJsonFile)
 
-    return json.dump(updated_json, output_file, indent=4)
-
-          # Write the updated JSON to the output directory
-          # output_file_path = os.path.join(output_dir, old_file_name)
-          # with open(output_file_path, 'w') as output_file:
-          #     json.dump(updated_json, output_file, indent=4)
-
-  def create_zip(self, output_dir, zip_filename):
-    shutil.make_archive(zip_filename, 'zip', output_dir)
-    self.downloadZip()
-    
-  def downloadZip(self, **event_args):
-    """This method is called when the downlaod button is clicked"""
-    # BlobMedia("text/plain", self.outputData, [name=output])
-    # if(self.outputFileName.text == ""):
-    #   self.outputFileName.text = "output"
-    outputFiles = anvil.BlobMedia(content_type="application/zip", content=self.output_dir, name="Output Files")
-    anvil.media.download(outputFiles)  
-  
-  def getData(self, **event_args):
-    """This method is called when the submit button is clicked"""
-    self.logs.text = ""
-    self.outputData.text = self.update_json(self.oldData, self.newData)
-    # self.outputData.text = (self.old_dir, self.new_dir)
-    pass
 
   def update_json(self, old_data, new_data):
 
-    old_data = json.loads(old_data.text)
-    new_data = json.loads(new_data.text)
+    old_data = json.dumps(old_data)
+    new_data = json.dumps(new_data)
     updated_data = self.update_json_values(old_data, new_data)
+    
     return json.dumps(updated_data, indent=4)
 
   def update_json_values(self, old_data, new_data):
@@ -75,16 +61,6 @@ class Form2(Form2Template):
           updated_data[key] = self.update_json_values(old_data[key], new_data[key])
         else:
           updated_data[key] = old_data[key]
-      # Discarded keys
-      for key in new_data:
-        if key not in old_data:
-          self.logs.text += "DISCARDED: " + "\n" + key + ": " + new_data[key] + "\n" + "\n"
-      #         updated_data[key] = new_data[key]
-      # Newly Added keys
-      for key in old_data:
-        if key not in new_data:
-          self.logs.text += "NEWLY ADDED: " + "\n" + key + ": " + old_data[key] + "\n" + "\n"
-      return updated_data
     elif isinstance(old_data, list) and isinstance(new_data, list):
       updated_data = []
       for i in range(min(len(old_data), len(new_data))):
@@ -95,32 +71,5 @@ class Form2(Form2Template):
     else:
       return new_data
 
-  def downloadData(self, **event_args):
-    """This method is called when the downlaod button is clicked"""
-    # BlobMedia("text/plain", self.outputData, [name=output])
-    if(self.outputFileName.text == ""):
-      self.outputFileName.text = "output"
-    updatedJsonFile = anvil.BlobMedia(content_type="text/plain", content=self.outputData.text.encode(), name=self.outputFileName.text+".json")
-    anvil.media.download(updatedJsonFile)
-
- 
-    
-  # def process_directory(self, old_files, new_files, output_files):
-  #   # os.makedirs(output_dir, exist_ok=True)
-    
-  #   # List all files in the old directory
-  #   # old_files = os.listdir(old_dir)
-    
-  #   for old_file_name in old_files:
-  #       if old_file_name in new_files:
-  #         for new_file_name in new_files:
-  #           if old_file_name == new_file_name:
-  #             old_json = json.load(old_file_name)
-  #             new_json = json.load(new_file_name)
-                
-  #             updated_json = update_json_values(old_json, new_json)
-              
-  #             # Write the updated JSON to the output directory
-  #             output_file_path = os.path.join(output_dir, old_file_name)
-  #             with open(output_file_path, 'w') as output_file:
-  #                 json.dump(updated_json, output_file, indent=4)
+  def goToForm1(self, **event_args):
+    open_form('Form1')
