c7cfe7de276f2506f6ff84478c7b53f8cf73b0d1
Jayavardhan Pathella <jpathella@opentext.com>
2023-10-11 21:10:39 +0530

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -32,9 +32,12 @@ class Form1(Form1Template):
       if isinstance(old_data, dict) and isinstance(new_data, dict):
           updated_data = {}
           for key in old_data:
-              if key in new_data:
-                  updated_data[key] = self.update_json_values(old_data[key], new_data[key])
+              if(isinstance(old_data[key], dict) or isinstance(old_data[key], list)):
+                updated_data[key] = self.update_json_values(old_data[key], new_data)
               else:
+                if old_data[key] in new_data:
+                  updated_data[key] = new_data[old_data[key]]
+                else:
                   updated_data[key] = old_data[key]
           return updated_data
       elif isinstance(old_data, list) and isinstance(new_data, list):
@@ -43,6 +46,14 @@ class Form1(Form1Template):
               updated_data.append(update_json_values(old_data[i], new_data[i]))
           if len(new_data) > len(old_data):
               updated_data.extend(new_data[len(old_data):])
+          return updated_data
+      elif isinstance(old_data, list) and isinstance(new_data, dict):
+          updated_data = []
+          for i in range(len(old_data)):
+            if(isinstance(old_data[i], dict) or isinstance(old_data[i], list)):
+              updated_data.append(self.update_json_values(old_data[i], new_data))
+            else:
+              updated_data.append(old_data[i])
           return updated_data
       else:
           return new_data
